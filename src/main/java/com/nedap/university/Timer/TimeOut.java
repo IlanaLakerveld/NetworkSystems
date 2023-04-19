b0fa29bf3739b137993a98e53adc5844cdc54054
ilana.lakerveld <i.d.lakerveld@student.utwente.nl>
2023-04-19 17:56:02 +0200

    ADDED documentation and checked warnings for Timer and exception

--- src/main/java/com/nedap/university/Timer/TimeOut.java
+++ src/main/java/com/nedap/university/Timer/TimeOut.java
@@ -8,19 +8,36 @@ import java.net.DatagramSocket;
 import java.util.Timer;
 
 /**
- * Set a timer for a time and after that time it calls a function TimeOut function.
+ * Set a timer for a time and after that time it calls a TimeOut function.
+ * The different constructors are for different time out cases The first is when timer is set during sending (part of) a file, the second for sending a request.
  */
 public class TimeOut {
     private Timer timer ;
     public int milliSeconds ;
     private int amount ;
 
-    public TimeOut(int milliSeconds, Sending send, DatagramPacket datagramPacket,int amount ){
+    /**
+     * This constructor is used when setting a timer wil sending a (part of) file
+     * When timer expires the TimeOutTask function will be called
+     * @param milliSeconds          Time in milliseconds before timer goes of
+     * @param send                  Class send (used in the TimeOutTask function)
+     * @param datagramPacket        Datagram packet that is sent
+     * @param amountOfResending     Amount of resends.
+     */
+    public TimeOut(int milliSeconds, Sending send, DatagramPacket datagramPacket,int amountOfResending ){
         timer = new Timer() ;
-        this.amount = amount ;
+        this.amount = amountOfResending ;
         this.milliSeconds = milliSeconds;
-        timer.schedule(new TimeOutTask(timer, send,datagramPacket, amount),milliSeconds);
+        timer.schedule(new TimeOutTask(timer, send,datagramPacket, amountOfResending),milliSeconds);
     }
+
+    /**
+     * Timer for sending a request. If the timer goes of, the TimeOutTaskRequest server is called.
+     * @param milliSeconds              Time in milliseconds before timer goes of
+     * @param socket                    Datagram socket
+     * @param datagramPacket            Datagram packet that is sent
+     * @param client                    Client that is sending the packet
+     */
     public TimeOut(int milliSeconds , DatagramSocket socket , DatagramPacket datagramPacket, Client client){
         timer = new Timer();
         this.milliSeconds = milliSeconds;
